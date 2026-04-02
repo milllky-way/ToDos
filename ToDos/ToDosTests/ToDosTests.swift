fd3359428551aa1bde290b58100616cf1259f14f
Дарья Саитова <dashasaitova1202@mail.ru>
2026-04-02 20:00:54 +0300

    Создаёт проект

---
+++ ToDos/ToDosTests/ToDosTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDosTests.swift
+//  ToDosTests
+//
+//  Created by Дарья Саитова on 02.04.2026.
+//
+
+import Testing
+@testable import ToDos
+
+struct ToDosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
